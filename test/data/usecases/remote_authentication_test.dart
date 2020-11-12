f0bb27fb9566cd3fac2f9d2f22bd09ca73b2779c
Carlos Henrique <rick_teloon@hotmail.com>
2020-11-12 10:29:33 -0300

    Move sut creation to a setup method

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -10,33 +10,39 @@ class RemoteAuthentication {
 
   RemoteAuthentication({@required this.httpClient, @required this.url});
   Future<void> auth() async {
-    await httpClient.request(url: url);
+    await httpClient.request(url: url, method: 'post');
   }
 }
 
 //Esta classe abstrata possui um método de request, porém como ela é abstrata não podemos instancia-la
 abstract class HttpClient {
-  Future<void> request({@required String url});
+  Future<void> request({@required String url, @required String method});
 }
 
 //Aqui usamos o mockito para criar uma classe que implementa o httpClient
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test("Should call httpClient with corect url", () async {
+  RemoteAuthentication sut;
+  HttpClientSpy httpClient;
+  String url;
+
+  setUp(() {
     //Instanciamos a classe Mockada
-    final httpClient = HttpClientSpy();
+    httpClient = HttpClientSpy();
     //Criamos uma url fake com o Faker
-    final url = faker.internet.httpUrl();
+    url = faker.internet.httpUrl();
     //Arrange
     //Intanciamos a classe que será testada e passamos os parâmetros
-    final sut = RemoteAuthentication(httpClient: httpClient, url: url);
+    sut = RemoteAuthentication(httpClient: httpClient, url: url);
+  });
+  test("Should call httpClient with corect values", () async {
     //Action
     //Chamando o auth
     await sut.auth();
     //Assert
     //Verificando se o request foi chamada com a url passada
-    verify(httpClient.request(url: url));
+    verify(httpClient.request(url: url, method: 'post'));
     // expect(actual, matcher)
   });
 }
