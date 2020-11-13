3bcbf4d1bcc8e005781c1182a4e6ba66cfef367f
Carlos Henrique <rick_teloon@hotmail.com>
2020-11-13 12:19:02 -0300

    move code do helper methods

--- lib/ui/pages/login/login_presenter.dart
+++ lib/ui/pages/login/login_presenter.dart
@@ -1,15 +1,15 @@
 abstract class LoginPresenter {
-  Stream get emailErrorStream;
-  Stream get passwordErrorStream;
-  Stream get isFormValidStream;
-  Stream get isLoadingStream;
-  Stream get mainErrorStream;
+  Stream<String> get emailErrorStream;
+  Stream<String> get passwordErrorStream;
+  Stream<bool> get isFormValidStream;
+  Stream<bool> get isLoadingStream;
+  Stream<String> get mainErrorStream;
 
   void validateEmail(String email);
 
   void validatePassword(String pasword);
 
-  void auth();
+  Future<void> auth();
 
   void dispose();
 }
--- test/ui/pages/login_pages_test.dart
+++ test/ui/pages/login_pages_test.dart
@@ -16,14 +16,16 @@ void main() {
   StreamController<bool> isFormValidController;
   StreamController<bool> isLoadingController;
   StreamController<String> mainErrorController;
-  Future<void> loadPage(WidgetTester tester) async {
-    presenter = LoginPresenterSpy();
+
+  void initStreams() {
     emailErrorController = StreamController<String>();
     passwordErrorController = StreamController<String>();
     isFormValidController = StreamController<bool>();
     isLoadingController = StreamController<bool>();
     mainErrorController = StreamController<String>();
+  }
 
+  void mockStreams() {
     when(presenter.emailErrorStream)
         .thenAnswer((_) => emailErrorController.stream);
 
@@ -38,17 +40,26 @@ void main() {
 
     when(presenter.mainErrorStream)
         .thenAnswer((_) => mainErrorController.stream);
-
-    final loginPage = MaterialApp(home: LoginPage(presenter));
-    await tester.pumpWidget(loginPage);
   }
 
-  tearDown(() {
+  void closeStreams() {
     emailErrorController.close();
     passwordErrorController.close();
     isFormValidController.close();
     isLoadingController.close();
     mainErrorController.close();
+  }
+
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = LoginPresenterSpy();
+    initStreams();
+    mockStreams();
+    final loginPage = MaterialApp(home: LoginPage(presenter));
+    await tester.pumpWidget(loginPage);
+  }
+
+  tearDown(() {
+    closeStreams();
   });
 
   testWidgets(
