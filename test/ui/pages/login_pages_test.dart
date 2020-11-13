8de56609747f6a2baa7d97f55908e786f4a2cb3d
Carlos Henrique <rick_teloon@hotmail.com>
2020-11-13 11:57:38 -0300

    ensure lofin page hiding loading

--- test/ui/pages/login_pages_test.dart
+++ test/ui/pages/login_pages_test.dart
@@ -71,6 +71,7 @@ void main() {
 
       final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
       expect(button.onPressed, null);
+      expect(find.byType(CircularProgressIndicator), findsNothing);
     },
   );
 
@@ -238,6 +239,21 @@ void main() {
       await tester.pump();
 
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    "Should hide loading",
+    (WidgetTester tester) async {
+      await loadPage(tester);
+
+      isLoadingController.add(true);
+      await tester.pump();
+
+      isLoadingController.add(false);
+      await tester.pump();
+
+      expect(find.byType(CircularProgressIndicator), findsNothing);
     },
   );
 }
